181b7edda01e0d4a4ffddf0fc9e11a2a11eb341d
KaanYagami <91686044+KaanYagami@users.noreply.github.com>
2022-01-29 00:17:11 +0300

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(document).ready(function () {
+    // LOADER
+    setTimeout(function () {
+      $(".content").fadeIn(800);
+      $(".loader-content").fadeOut(300);
+    }, 800);
+  });
+});
